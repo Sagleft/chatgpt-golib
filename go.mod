dc394b6ee0925b0add731b86ae662709a6e4bc00
Sagleft <sagleft@inbox.ru>
2023-03-20 18:39:54 +0300

    upload code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Sagleft/chatgpt-golib
+
+go 1.18
+
+require github.com/PullRequestInc/go-gpt3 v1.1.13
---
+++ main.go
@@ -0,0 +1,85 @@
+package gptlib
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/PullRequestInc/go-gpt3"
+)
+
+type Client interface {
+	Request(RequestData) (string, error)
+}
+
+type RequestData struct {
+	// required
+	Prompt string
+
+	// optional
+	MaxTokens int    // by default: 1000
+	Role      string // by default: user
+	Timeout   time.Duration
+	UserID    string
+}
+
+func NewChatGPT(openAIAPIToken string) Client {
+	return &chatGPT{
+		conn: gpt3.NewClient(openAIAPIToken),
+	}
+}
+
+type chatGPT struct {
+	conn gpt3.Client
+}
+
+func (c *chatGPT) Request(data RequestData) (string, error) {
+	if data.Role == "" {
+		data.Role = "user"
+	}
+	if data.MaxTokens == 0 {
+		data.MaxTokens = 1000
+	}
+
+	var ctx context.Context
+	var ctxCancel func()
+	if data.Timeout == 0 {
+		ctx = context.Background()
+	} else {
+		ctx, ctxCancel = context.WithTimeout(context.Background(), data.Timeout)
+	}
+	defer ctxCancel()
+
+	response, err := c.conn.ChatCompletion(
+		ctx,
+		gpt3.ChatCompletionRequest{
+			Messages: []gpt3.ChatCompletionRequestMessage{
+				{
+					Role:    data.Role,
+					Content: data.Prompt,
+				},
+			},
+			Temperature:      0.6,
+			MaxTokens:        data.MaxTokens,
+			TopP:             1,
+			N:                1,
+			FrequencyPenalty: 1,
+			PresencePenalty:  1,
+			User:             data.UserID,
+		},
+	)
+	if err != nil {
+		return "", fmt.Errorf("send chat completion request: %w", err)
+	}
+
+	dataArray := []string{}
+	for _, data := range response.Choices {
+		if data.Message.Content != "" {
+			dataArray = append(dataArray, data.Message.Content)
+		}
+	}
+
+	result := strings.Join(dataArray, "\n")
+	return strings.TrimLeft(result, "\n"), nil
+}
